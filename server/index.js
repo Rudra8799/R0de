395ac068c6fea1d7d8b3a75aca3ae95d999ca92a
Rudra8799 <rudrashekhawat44@gmail.com>
2024-10-08 03:47:12 +0530

    second commit

--- server/index.js
+++ server/index.js
@@ -79,10 +79,10 @@ const port = process.env.PORT || 5000;
 
 const __dirname = path.resolve();
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/client/rcode/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "client","rcode", "dist", "index.html"));
 });
 
 server.listen(port, () => {
